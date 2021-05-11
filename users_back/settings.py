256915d62c00da700584cfa9b61ca9ffe1ee12d0
Erasmo Jr <erasmo.cjunior@gmail.com>
2021-05-11 17:42:57 -0300

    Disable browseable API

--- users_back/settings.py
+++ users_back/settings.py
@@ -51,6 +51,12 @@ CORS_ALLOWED_ORIGINS = [
     "https://editlater.com"
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES' : (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
